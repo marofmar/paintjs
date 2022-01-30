2e0f82b2cfc11d31b83639a92f37fd9a233bfa17
marofmar <yujin.mar.chung@gmail.com>
2022-01-30 23:32:32 +0900

    featrue add: stroke size change

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor")
+const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode")
 
 canvas.width = 700;
 canvas.height = 700;
@@ -10,6 +12,7 @@ ctx.lineWidth = 2.5;
 
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false
@@ -37,6 +40,21 @@ function handleColorClick(event) {
     ctx.strokeStyle = color;  /*override the value of the stroke color*/
 }
 
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if (filling == true) {
+        filling = false;
+        mode.innerText = "Fill"
+    } else {
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -44,4 +62,12 @@ if (canvas) {
     canvas.addEventListener("mouseleave", stopPainting);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+if (range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+Array.from(colors).forEach(color=> color.addEventListener("click", handleColorClick));
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
+}
